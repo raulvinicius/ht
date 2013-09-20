7b54201343d67a3facfc951f5e219c8c5272947f
unknown <Raissa@Raissa-HP.(none)>
2013-09-20 20:18:11 -0300

    metade dos cases - 20/09/13

--- wp-content/themes/hightouch/frontend/cases.php
+++ wp-content/themes/hightouch/frontend/cases.php
@@ -1,1 +1,154 @@
-<h1>frontent Cases</h1>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/style.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/bootstrap.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/bootstrap-responsive.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+
+
+
+            <header>
+                <div id="header" class="container-fluid navbar-fixed-top">
+                    <div class="row-fluid">
+
+                        <div id="bg-logo" class="span3">
+                            <a id="logo" href="#">
+                                <h1>hightouch</h1>
+                                <h2>Viva sua empresa</h2>
+                            </a>
+                        </div>
+
+                        <div id="menu" class="span9 menu">
+                            <nav>
+                                <ul>
+                                    <li><a href="#">[hi]</a></li>
+                                    <li><a href="#">Cases</a></li>
+                                    <li><a href="#">Soluções</a></li>
+                                    <li><a href="#">Contato</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+
+                    </div>
+                </div>
+            </header>
+
+
+        <section id="hightouch">
+
+            <section>
+                <div id="wrap-case-principal" class="container-fluid">
+                    <div class="row-fluid">
+
+                        <div id="img-case" class="img-topo">
+                            <div id="mascara-azul">
+
+                                <div class="figcaption">
+                                    <h2>Encontro Leonardo da Vinci</h2>
+                                    <h3>2013</h3>
+                                    <p>Podemos já vislumbrar o modo pelo qual o novo modelo estrutural aqui preconizado assume importantes posições no estabelecimento do impacto na agilidade decisória.</p>
+                                    <a href="#" class="ver-mais-red"><i class="radius-left" ></i><p class="radius" >ver case</p></a>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <section>   
+                <div></div>
+            </section>
+
+
+
+
+
+            <footer>
+                <div id="footer" class="container-fluid">
+                    <div class="row-fluid">
+
+                        <a href="#" class="icone-fale">
+                            <i class="radius-left"><img src="<?php bloginfo("template_url") ?>/img/hi-icon-fale.png"></i>
+                            <p class="radius">Fale com a gente</p>
+                        </a>
+
+                        <div class="address">
+                            <address>
+                                <p>Centro Empresarial Santa Cruz </br>
+                                    SEPS 705/905 Bl. A Sala 142</br>
+                                    72390-055 - Brasília - DF</br>
+                                    <span>Tel: <strong>61.3326.2201</strong></span>
+                                </p>
+                            </address>
+                        </div>
+
+                    </div>
+                </div>
+            </footer>
+            
+
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- 
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+         -->
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        
+<!-- 
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+ -->
+    </body>
+</html>
